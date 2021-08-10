9faf0a824039e3245d3f6254853089bca88a7e84
KevinJonathan117 <kevinjonathan3010@gmail.com>
2021-08-10 15:02:04 +0700

    connected outlets and actions

---
+++ Today/ReminderListCell.swift
@@ -0,0 +1,22 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by Kevin Jonathan on 10/08/21.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    typealias DoneButtonAction = () -> Void
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
+    var doneButtonAction: DoneButtonAction?
+    
+    @IBAction func doneButtonTriggered (_ sender: UIButton) {
+        doneButtonAction?()
+    }
+}
